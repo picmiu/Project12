fc81b24f2e0d6a0e8154b53203f9aaab8390b6e0
admin <admin@YD02-15>
2020-11-09 17:21:35 +0900

    ㅎㅇ 오늘꺼 복습 하셈 ㅋ

---
+++ ajaxProject/src/com/yedam/comments/DAO.java
@@ -0,0 +1,36 @@
+package com.yedam.comments;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DAO {
+	protected Connection conn;
+	protected Statement stmt;
+	protected PreparedStatement pstmt;
+	protected ResultSet rs;
+
+	public void connect() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "hr", "hr");
+
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void disconnect() {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
